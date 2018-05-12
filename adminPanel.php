26388195b8732564c104c6225d393dfd3d13b1e3
mujoh <m.hadzimehanovic@gmail.com>
2018-05-12 12:38:16 +0200

    add item on admin panel added

--- adminPanel.php
+++ adminPanel.php
@@ -76,4 +76,90 @@
         <li id="logout"><a href="controller/logout.php">Log Out</a></li>
       </ul><br>
     </div>
+
+	<!--ADD ITEM -->
+	<div class="col-sm-9">
+		<div id="jedan" style="display:none;">
+			<hr style="border-color:#ddd;">
+			<h2 style="font-weight:bold;margin-top:20px;">Add Items</h2>
+			<hr style="border-color:#ddd;">
+		<div class="well" style="margin-right:100px;">
+		<h3>Adding Item</h3>
+		<hr>
+		<form id="formAdd" method="POST">
+		
+		  <div class="form-group row">
+			<label for="itemname" class="col-sm-2 col-form-label">Item Name</label>
+			<div class="col-sm-10">
+			  <input type="text" name ="name" class="form-control" id="name" placeholder="Item Name" required>
+			</div>
+		  </div>
+		  
+		  <div class="form-group row">
+			<label for="itemcategory" class="col-sm-2 col-form-label">Item Brand</label>
+			<div class="col-sm-10">
+				<select class="form-control" name = "brand" id="brand" required>
+					<option value="">Select Brand</option>
+					<option value="1">Samsung</option>
+					<option value="2">Apple</option>
+					<option value="3">Nokia</option>
+					<option value="4">Sony</option>
+					<option value="5">LG</option>
+					<option value="6">HP</option>
+					<option value="7">IBM</option>
+					<option value="8">Lenovo</option>
+					<option value="9">Microsoft</option>
+					<option value="10">Unknown</option>
+				  </select>
+			</div>
+		  </div>
+		  
+		  <div class="form-group row">
+			<label for="itemprice" class="col-sm-2 col-form-label">Item Price</label>
+			<div class="col-sm-10">
+			  <input type="number" name ="price" class="form-control" id="price" placeholder="Item Price" required>
+			</div>
+		  </div>
+		  
+		  <div class="form-group row">
+			<label for="quantity" class="col-sm-2 col-form-label">Item Quantity</label>
+			<div class="col-sm-10">
+				<input type="number" name="quantity" id="quantity" class="form-control" placeholder="Item Quantity" required>
+			</div>
+		  </div>
+		  
+		  <div class="form-group row">
+			<label for="capacity" class="col-sm-2 col-form-label">Item Capacity</label>
+			<div class="col-sm-10">
+				<input type="number" name="capacity" id="capacity" class="form-control" placeholder="Item Capacity" required>
+			</div>
+		  </div>
+
+		  <div class="form-group row">
+			<label for="color" class="col-sm-2 col-form-label">Item Color</label>
+			<div class="col-sm-10">
+				<input type="text" name="color" id="color" class="form-control" placeholder="Item Color" required>
+			</div>
+		  </div>		  
+		  
+		  <div class="form-group row">
+			<label for="itempicture" class="col-sm-2 col-form-label">Item Picture</label>
+			<div class="col-sm-10">
+			    <input id="img" name="img" type="file" class="file" required>
+			</div>
+		  </div>
+		  
+		  <div class="form-group row">
+			<label for="comment" class="col-sm-2 col-form-label">Details About Item</label>
+			<div class="col-sm-10">
+				<textarea rows="5" id="comment" placeholder="separate each line with enter" class="form-control input rounded input-xlarge"></textarea>
+			</div>
+		  </div>
+		  
+			<button type="submit"class="btn btn-primary" id="add" style="margin-top:20px;">Add Item ! </button>
+		</form>
+		
+		</div> <!--close well-->
+		</div> <!--Add item end -->
+</body>
 	
