e3e153505d9fc37c312abd1d7bcff48ee6106012
mujoh <m.hadzimehanovic@gmail.com>
2018-05-14 11:57:44 +0200

    updating item added

--- process.php
+++ process.php
@@ -17,4 +17,16 @@
 		$queryAdd = ("INSERT INTO products (product_name, product_brand, product_quantity, product_price, product_capacity, product_color, product_image, product_comment) VALUES ('$name', '$brand', '$quantity', '$price', '$capacity', '$color', '$img', '$comment')");
 		$result = mysqli_query($con, $queryAdd);
 	}
+
+	else if($action=="update")
+	{
+		$idUpdate = $_POST['id'];
+		$nameUpdate = $_POST['name'];
+		$priceUpdate = $_POST['price'];
+		$quantityUpdate = $_POST['quantity'];
+		$detailsUpdate = $_POST['comment'];
+		
+		$queryUpdate = "UPDATE products SET product_name='$nameUpdate', product_price='$priceUpdate', product_quantity='$quantityUpdate', product_comment='$detailsUpdate' WHERE product_id=".$idUpdate;
+		mysqli_query($con, $queryUpdate);
+	}
 ?>
