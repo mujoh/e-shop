ca8211fffea51938ed9bdde79da81200444397d2
Idrizovich <dino.i@hotmail.com>
2018-05-15 20:05:27 +0200

    delivery updated

---
+++ controller/delivery.php
@@ -0,0 +1,58 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Moby Electronic d.o.o.</title>
+
+	<!-- Latest compiled and minified CSS -->
+
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<!-- Google Font -->
+
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Raleway:400,300,500,700,600' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css" type="text/css">
+
+    <!-- Theme Stylesheet -->
+
+    <link rel="stylesheet" href="css/styles.css">
+
+    <link rel="stylesheet" href="css/responsive.css">
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="js/skriptaIndex.js"></script>
+	
+	
+</head>
+<body>
+	<?php
+		include_once('view/topbar.html');
+		include_once('view/header.html');
+	?>
+    
+<div id="mainBody">
+	<div class="container">
+		<div class="row" style="margin-left:-30px;margin-bottom:50px;">
+			<img src="img/delivery.png">
+		</div>
+	</div>
+</div>
+
+<div class="footerGet">
+	<?php
+		include_once('view/footer.html');
+	?>
+</div>
+
+</body>
+</html>
