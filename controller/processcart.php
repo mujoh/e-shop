1cd2c0f0db3c3353c08a4cc03c9fa6dab93f2a8d
lejlakrajinic <lejla.krajinic@gmail.com>
2018-05-16 17:28:47 +0200

    upgraded processcart

--- controller/processcart.php
+++ controller/processcart.php
@@ -24,6 +24,32 @@ if(isset($_POST["add"]))
 			echo '<script>alert("Products already added to cart")</script>';
 			echo '<script>window.location="../index.php"</script>';
 		}
-    }
-    
-    ?>
+	}
+	else
+	{
+		$item_array = array(
+		'product_id' => $_GET["id"],
+		'item_name' => $_POST["hidden_name"],
+		'product_price' => $_POST["hidden_price"],
+		'item_quantity' => $_POST["quantity"]
+		);
+		$_SESSION["cart"][0] = $item_array;
+		echo '<script>window.location="../index.php"</script>';
+	}
+}
+if(isset($_GET["action"]))
+{
+	if($_GET["action"] == "delete")
+	{
+		foreach($_SESSION["cart"] as $keys => $values)
+		{
+			if($values["product_id"] == $_GET["id"])
+			{
+				unset($_SESSION["cart"][$keys]);
+				echo '<script>alert("Product has been removed")</script>';
+				echo '<script>window.location="../cart.php"</script>';
+			}
+		}
+	}
+}
+?>
