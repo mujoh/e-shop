94f199308855642162830c40d9963e12083f25f4
mujoh <m.hadzimehanovic@gmail.com>
2018-05-31 17:20:10 +0200

    update form

--- update.php
+++ update.php
@@ -57,11 +57,64 @@
             </div>
         </div>
 </div>
-
-<div id="mainBody">
-    <h1 style="text-align:center;">Update Form</h1>
     
+<div id="mainBody">
+	<h1 style="text-align:center;">Update Form</h1>
+	
+	<?php
+		include 'controller/connect.php';
+		
+		$id = $_GET['id'];
+		$query = "SELECT * FROM products WHERE product_id=$id";
+		$result = mysqli_query($con, $query);
+		while($row = mysqli_fetch_array($result)){
+			
+			$name = $row['product_name'];
+			$price = $row['product_price'];
+			$quantity = $row['product_quantity'];
+			$comment = $row['product_comment'];
+	?>
+		<form id="formUpdate" method="POST">
+			  <div class="form-group row">
+				<label for="itemid" class="col-sm-2 col-form-label">Item ID</label>
+				<div class="col-sm-10">
+				  <input type="number" name ="id-update" value="<?= $id;?>" class="form-control" id="id-update" required>
+				</div>
+			  </div>
+			  
+				
+			  <div class="form-group row">
+				<label for="itemname" class="col-sm-2 col-form-label">Item Name</label>
+				<div class="col-sm-10">
+				  <input type="text" name ="name-update" value="<?= $name;?>" class="form-control" id="name-update" required>
+				</div>
+			  </div>
+			  
+			  <div class="form-group row">
+				<label for="itemprice" class="col-sm-2 col-form-label">Item Price</label>
+				<div class="col-sm-10">
+				  <input type="number" name ="price-update" value="<?= $price;?>" class="form-control" id="price-update" required>
+				</div>
+			  </div>
+			  
+			  <div class="form-group row">
+				<label for="quantity" class="col-sm-2 col-form-label">Item Quantity</label>
+				<div class="col-sm-10">
+					<input type="number" name="quantity-update" value="<?= $quantity;?>" id="quantity-update" class="form-control" required>
+				</div>
+			  </div>
+			  
+			  <div class="form-group row">
+				<label for="comment" class="col-sm-2 col-form-label">Details About Item</label>
+				<div class="col-sm-10">
+					<textarea rows="5" id="comment-update" class="form-control input rounded input-xlarge"><?= $comment;?></textarea>
+				</div>
+			  </div>
+	<?php
+		}
+	?>
+				<button type="submit" class="btn btn-primary" id="update" style="margin-top:20px;">Update Item ! </button>
+		</form>
 </div>
-
 </body>
 </html>
